9a0370d95820e12b8534a37dd1f7506ab34d483d
王召东 <342792314@qq.com>
2017-11-12 09:32:54 +0800

    Add files via upload

---
+++ include/read_file.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <dirent.h>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int read_file( vector<char*> *files_name , const char* FILE_PATH);
